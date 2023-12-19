a2f3330ced1160cb61fa215856acc2644d1044b9
Cobus <cobus@binarymoon.co.za>
2023-12-19 13:59:45 +1000

    Add (failing) visa tests

--- ConnX.UnitTests/CardTypeTests/VisaTests.cs
+++ ConnX.UnitTests/CardTypeTests/VisaTests.cs
@@ -2,5 +2,116 @@
 {
     public class VisaTests
     {
+        [Fact]
+        public void CardNumber_ThatStartsWith4AndIs13CharactersLong_IsValid()
+        {
+            //arrange
+            var creditCard = new CreditCard("4123456789012");
+            var visaRule = new VisaRule(creditCard);
+
+            //act
+            var result = visaRule.Check();
+
+            //assert
+            result.IsValid.ShouldBe(true);
+        }
+
+        [Fact]
+        public void CardNumber_ThatStartsWith4AndIs14CharactersLong_IsValid()
+        {
+            //arrange
+            var creditCard = new CreditCard("41234567890124");
+            var visaRule = new VisaRule(creditCard);
+
+            //act
+            var result = visaRule.Check();
+
+            //assert
+            result.IsValid.ShouldBe(true);
+        }
+
+        [Fact]
+        public void CardNumber_ThatStartsWith4AndIs15CharactersLong_IsValid()
+        {
+            //arrange
+            var creditCard = new CreditCard("412345678901268");
+            var visaRule = new VisaRule(creditCard);
+
+            //act
+            var result = visaRule.Check();
+
+            //assert
+            result.IsValid.ShouldBe(true);
+        }
+
+        [Fact]
+        public void CardNumber_ThatStartsWith4AndIs16CharactersLong_IsValid()
+        {
+            //arrange
+            var creditCard = new CreditCard("4123456789012154");
+            var visaRule = new VisaRule(creditCard);
+
+            //act
+            var result = visaRule.Check();
+
+            //assert
+            result.IsValid.ShouldBe(true);
+        }
+
+        [Theory]
+        [InlineData("1212345678901231")]
+        [InlineData("9912345678901232")]
+        [InlineData("2812345678901233")]
+        [InlineData("5512345678901234")]
+        [InlineData("6112345678901235")]
+        [InlineData("7412345678901236")]
+        public void CardNumber_ThatDoesNotStartWith4AndIs16CharactersLong_IsInValid(string number)
+        {
+            //arrange
+            var creditCard = new CreditCard(number);
+            var visaRule = new VisaRule(creditCard);
+
+            //act
+            var result = visaRule.Check();
+
+            //assert
+            result.IsValid.ShouldBe(false);
+            result.Error.ShouldBeAssignableTo<CardTypeStartsWithError>();
+        }
+
+        [Theory]
+        [InlineData("4445678903")]
+        [InlineData("4768161234567890123")]
+        public void CardNumber_ThatDoesStartWith4AndIsNot13To16CharactersLong_IsInValid(string number)
+        {
+            //arrange
+            var creditCard = new CreditCard(number);
+            var visaRule = new VisaRule(creditCard);
+
+            //act
+            var result = visaRule.Check();
+
+            //assert
+            result.IsValid.ShouldBe(false);
+            result.Error.ShouldBeAssignableTo<CardTypeLengthError>();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("6846846851")]
+        public void CardNumber_ThatDoesNotStartWith4AndIsNot13To16CharactersLong_IsInValid(string number)
+        {
+            //arrange
+            var creditCard = new CreditCard(number);
+            var visaRule = new VisaRule(creditCard);
+
+            //act
+            var result = visaRule.Check();
+
+            //assert
+            result.IsValid.ShouldBe(false);
+            result.Error.ShouldBeAssignableTo<CardTypeLengthError>();
+        }
     }
 }
