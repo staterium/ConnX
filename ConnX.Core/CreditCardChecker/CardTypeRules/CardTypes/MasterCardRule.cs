af9c495a7e18b7dfe6457f278c98313e0d33dd13
Cobus <cobus@binarymoon.co.za>
2023-12-19 13:56:12 +1000

    Make MasterdCard tests pass

--- ConnX.Core/CreditCardChecker/CardTypeRules/CardTypes/MasterCardRule.cs
+++ ConnX.Core/CreditCardChecker/CardTypeRules/CardTypes/MasterCardRule.cs
@@ -14,7 +14,35 @@ namespace ConnX.Core.CreditCardChecker.CardTypeRules.CardTypes
 
         public CardTypeValidationResult Check()
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(CreditCard.Number))
+            {
+                return new CardTypeValidationResult(
+                    startsWithRightNumbers: false,
+                    isRightLength: false,
+                    error: new CardTypeLengthError(CardType));
+            }
+
+            if (CreditCard.Number.Length != 16)
+            {
+                return new CardTypeValidationResult(
+                    startsWithRightNumbers: false,
+                    isRightLength: false,
+                    error: new CardTypeLengthError(CardType));
+            }
+
+            var firstTwoDigits = int.Parse(CreditCard.Number[0..2]);
+
+            if (firstTwoDigits < 51 || firstTwoDigits > 55)
+            {
+                return new CardTypeValidationResult(
+                    startsWithRightNumbers: false,
+                    isRightLength: false,
+                    error: new CardTypeStartsWithError(CardType));
+            }
+
+            return new CardTypeValidationResult(
+                    startsWithRightNumbers: true,
+                    isRightLength: true);
         }
     }
 }
