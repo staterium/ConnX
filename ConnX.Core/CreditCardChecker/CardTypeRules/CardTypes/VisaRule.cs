701cccf6cba013873a572d7842e921fa365ed693
Cobus <cobus@binarymoon.co.za>
2023-12-19 14:00:39 +1000

    Make visa tests pass

--- ConnX.Core/CreditCardChecker/CardTypeRules/CardTypes/VisaRule.cs
+++ ConnX.Core/CreditCardChecker/CardTypeRules/CardTypes/VisaRule.cs
@@ -14,7 +14,33 @@ namespace ConnX.Core.CreditCardChecker.CardTypeRules.CardTypes
 
         public CardTypeValidationResult Check()
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(CreditCard.Number))
+            {
+                return new CardTypeValidationResult(
+                    startsWithRightNumbers: false,
+                    isRightLength: false,
+                    error: new CardTypeLengthError(CardType));
+            }
+
+            if (CreditCard.Number.Length < 13 || CreditCard.Number.Length > 16)
+            {
+                return new CardTypeValidationResult(
+                    startsWithRightNumbers: false,
+                    isRightLength: false,
+                    error: new CardTypeLengthError(CardType));
+            }
+
+            if (CreditCard.Number[0..1] != "4")
+            {
+                return new CardTypeValidationResult(
+                    startsWithRightNumbers: false,
+                    isRightLength: false,
+                    error: new CardTypeStartsWithError(CardType));
+            }
+
+            return new CardTypeValidationResult(
+                    startsWithRightNumbers: true,
+                    isRightLength: true);
         }
     }
 }
